c1cfc9153ec11df420d9ebe3ce1c58c8f63f872c
YASH SOMPURA <sompurayash@live.com>
2018-01-26 16:06:37 -0500

    Jackpot and Cash Function added

--- SlotMachine/ViewController.swift
+++ SlotMachine/ViewController.swift
@@ -7,17 +7,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var jackpotValue = 5000
+    var cashValue = 200
+    
+    @IBOutlet weak var quitButton: UIButton!
+    @IBOutlet weak var jackpotLabel: UILabel!
+    @IBOutlet weak var cashLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "background.png")!)
-    
+        
+        jackpotLabel.text = String(jackpotValue)
+        cashLabel.text = String(cashValue)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func quitButtonAction(_ sender: UIButton) {
+        exit(0);
+    }
+    
+    
 }
 
