f1e0530a8499362bacebaf232df4001eee3a80ae
YASH SOMPURA <sompurayash@live.com>
2018-02-05 22:56:44 -0500

    Project Completed

--- SlotMachine/ViewController.swift
+++ SlotMachine/ViewController.swift
@@ -4,11 +4,16 @@
 // Description: Advance iOS Programming Assignment 1
 // Version: 0.0.1
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
     var jackpotValue = 5000
     var cashValue = 200
+    
+    var slotsound = NSURL(fileURLWithPath: Bundle.main.path(forResource: "spin", ofType: "wav")!)
+    var audio = AVAudioPlayer()
+    
     
     var ace = 0
     var king = 0
@@ -26,6 +31,7 @@ class ViewController: UIViewController {
     var eightResult = [18,22,26,30]
     var nineResult = [19,23,27,31]
     var tenResult = [20,24,28,32]
+     let images = [#imageLiteral(resourceName: "1"),#imageLiteral(resourceName: "2"),#imageLiteral(resourceName: "3"),#imageLiteral(resourceName: "4"),#imageLiteral(resourceName: "6"),#imageLiteral(resourceName: "7"),#imageLiteral(resourceName: "8"),#imageLiteral(resourceName: "9"),#imageLiteral(resourceName: "10"),#imageLiteral(resourceName: "11"),#imageLiteral(resourceName: "12"),#imageLiteral(resourceName: "13"),#imageLiteral(resourceName: "14"),#imageLiteral(resourceName: "15"),#imageLiteral(resourceName: "16"),#imageLiteral(resourceName: "17"),#imageLiteral(resourceName: "18"),#imageLiteral(resourceName: "19"),#imageLiteral(resourceName: "20"),#imageLiteral(resourceName: "21"),#imageLiteral(resourceName: "22"),#imageLiteral(resourceName: "23"),#imageLiteral(resourceName: "24"),#imageLiteral(resourceName: "25"),#imageLiteral(resourceName: "26"),#imageLiteral(resourceName: "27"),#imageLiteral(resourceName: "28"),#imageLiteral(resourceName: "29"),#imageLiteral(resourceName: "30"),#imageLiteral(resourceName: "31"),#imageLiteral(resourceName: "32")]
     
     @IBOutlet weak var quitButton: UIButton!
     @IBOutlet weak var jackpotLabel: UILabel!
@@ -43,6 +49,15 @@ class ViewController: UIViewController {
         
         jackpotLabel.text = String(jackpotValue)
         cashLabel.text = String(cashValue)
+        self.slotPicker.delegate = self
+        self.slotPicker.dataSource = self
+        
+        do{
+            audio = try AVAudioPlayer(contentsOf: slotsound as URL)
+            audio.prepareToPlay()
+        }catch{
+            
+        }
         
         Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(onTimerEvent(timer:)), userInfo: nil, repeats: true)
     }
@@ -73,10 +88,6 @@ class ViewController: UIViewController {
         queen = 0
         jack = 0
         
-        self.firstCard.image = UIImage(named: "backSide")
-        self.secondCard.image = UIImage(named: "backSide")
-        self.thirdCard.image = UIImage(named: "backSide")
-        self.resultImage.image = UIImage(named: "")
         
     }
     
@@ -88,19 +99,19 @@ class ViewController: UIViewController {
         var thirdRandomNumber : Int
         var res : Bool
         
-        var fileRes : String
+        audio.play()
+        
+        
       
         firstRandomNumber = Int(arc4random_uniform(32) + 1)
-        fileRes = String(firstRandomNumber)
-        self.firstCard.image = UIImage(named: fileRes)
+        print ("first Random Number" + String(firstRandomNumber))
         
         secondRandomNumber = Int(arc4random_uniform(32) + 1)
-        fileRes = String(secondRandomNumber)
-        self.secondCard.image = UIImage(named: fileRes)
+        print ("second Rand Num " + String(secondRandomNumber))
         
         thirdRandomNumber = Int(arc4random_uniform(32) + 1)
-        fileRes = String(thirdRandomNumber)
-        self.thirdCard.image = UIImage(named: fileRes)
+        print ("third" + String(thirdRandomNumber))
+        rollslot(firstNumber:firstRandomNumber, secondNumber: secondRandomNumber, thirdNumber: thirdRandomNumber)
         
         if aceResult.contains(firstRandomNumber)    {
             ace += 1
@@ -289,7 +300,46 @@ class ViewController: UIViewController {
         
     }
     
+    func rollslot(firstNumber : Int,secondNumber:Int, thirdNumber:Int){ // roll pickerview
+        var delay : TimeInterval = 0
+        
+        print ("first " + String(firstNumber))
+        let first =  firstNumber - 2
+        let second =  secondNumber - 2
+        let third =  thirdNumber - 2
+        // iterate over each component, set random img
+        for i in 0..<slotPicker.numberOfComponents{
+            DispatchQueue.main.asyncAfter(deadline: .now() + delay, execute: {
+                if (i == 0) {
+                    self.slotPicker.selectRow(first, inComponent: i, animated: true)
+                }
+                else if (i == 1){
+                    self.slotPicker.selectRow(second, inComponent: i, animated: true)
+                }
+                else {
+                    self.slotPicker.selectRow(third, inComponent: i, animated: true)
+                }
+            })
+            delay += 0.2
+        }
+    }
     
     
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 3
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return images.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        let index = row % images.count
+        return UIImageView(image:images[index])
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return images[component].size.height + 1
+    }
 }
 
